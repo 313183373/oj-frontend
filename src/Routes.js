323dcf25f68693d097a245805b9ac31132a1e289
kaixin.liu@thoughtworks.com <kaixin.liu@CNkaixin.liu>
2019-03-01 17:37:28 +0800

    fixed router

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {HashRouter, Route, Switch} from 'react-router-dom';
-
+import Problem from './pages/problem';
 import Home from './pages/Home';
 
 const Routes = () => (
     <HashRouter>
-        <Switch>
-            <Route exact path="/" component={Home}/>
-        </Switch>
+        <Home/>
     </HashRouter>
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import {view as TopAppBar} from '../home/';
 import { Switch, Route } from 'react-router-dom';
+import Problem from './problem';
 import Problems from './problems';
-import Problem from './problem'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import theme from '../theme';
+
 
 const Home = () => {
     return (
         <MuiThemeProvider theme={theme}>
             <TopAppBar/>
             <Switch>
-                <Route path='/problems' component={Problems}/>
+                <Route exact path='/' component={Problems}/>
                 <Route path='/problem/:id' component={Problem}/>
             </Switch>
         </MuiThemeProvider>
