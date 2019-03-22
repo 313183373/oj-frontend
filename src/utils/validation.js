40ce53438b8b5c35b891ede5ee0d0c7f5bcb4496
xion <313183373@qq.com>
2019-03-22 23:45:06 +0800

    add usernameValidatot

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,6 +1,5 @@
 import isEmail from 'validator/lib/isEmail'
 import PasswordValidator from "password-validator";
-import validator from 'validator';
 
 export function emailValidator(value) {
   return value && !isEmail(value.trim()) ? 'Invalid email' : null;
@@ -14,7 +13,7 @@ export function required(requiredFields, values) {
   return requiredFields.reduce(
     (fields, field) => ({
       ...fields,
-      ...(isDirty(values[field]) ? undefined : { [field]: 'Required' }),
+      ...(isDirty(values[field]) ? undefined : {[field]: 'Required'}),
     }),
     {},
   );
@@ -29,5 +28,6 @@ export function passwordValidator(password) {
 }
 
 export function usernameValidator(username) {
-  // return validator.
+  const regex = /^[\w-]{5,30}$/;
+  return regex.test(username);
 }
