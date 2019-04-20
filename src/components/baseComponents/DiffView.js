6850fb68d554fc0bec7d71afc999417a123819bd
xion <313183373@qq.com>
2019-04-20 19:20:46 +0800

    make react-diff-view to display more details

--- src/components/baseComponents/DiffView.js
+++ src/components/baseComponents/DiffView.js
@@ -2,47 +2,62 @@ import {Diff, Hunk, parseDiff} from "react-diff-view";
 import 'react-diff-view/style/index.css';
 import React from "react";
 
-function getChangeKey(lineNumber) {
-  return `I${lineNumber}`;
+// function getChangeKey(lineNumber) {
+//   return `I${lineNumber}`;
+// }
+
+const stringifyContent = hunks => {
+  return hunks.map(hunk => {
+    return {
+      ...hunk,
+      changes: hunk.changes.map(change => ({...change, content: JSON.stringify(change.content).slice(1, -1)})),
+    }
+  })
+}
+
+const getStringifiedFiles = files => {
+  return files.map(file => ({
+    ...file,
+    hunks: stringifyContent(file.hunks),
+  }))
 }
 
 export const DiffView = ({diffText}) => {
   const files = parseDiff(diffText, {nearbySequences: 'zip'});
-
-  const getWidgets = (hunks) => {
-    const changes = hunks.flatMap(hunk => {
-      return [...hunk.changes];
-    }).filter(change => change.type !== 'normal');
-    const compareLine = changes.reduce((result, change) => {
-      const line = change.lineNumber;
-      if (result[line]) {
-        result[line].push(change.content);
-      } else {
-        result[line] = [change.content];
-      }
-      return result;
-    }, {});
-    let hasReturnInLine = {};
-    for (const [lineNumber, line] of Object.entries(compareLine)) {
-      if (line.length === 2) {
-        if (line[0] === line[1]) {
-          hasReturnInLine[getChangeKey(lineNumber)] = <span style={{color: 'red'}}>maybe a '\n' difference</span>;
-        }
-      }
-    }
-    return hasReturnInLine;
-  };
+  // const getWidgets = (hunks) => {
+  //   const changes = hunks.flatMap(hunk => {
+  //     return [...hunk.changes];
+  //   }).filter(change => change.type !== 'normal');
+  //   const compareLine = changes.reduce((result, change) => {
+  //     const line = change.lineNumber;
+  //     if (result[line]) {
+  //       result[line].push(change.content);
+  //     } else {
+  //       result[line] = [change.content];
+  //     }
+  //     return result;
+  //   }, {});
+  //   let hasReturnInLine = {};
+  //   for (const [lineNumber, line] of Object.entries(compareLine)) {
+  //     if (line.length === 2) {
+  //       console.log(JSON.stringify(line[0]), JSON.stringify(line[1]));
+  //       if (line[0] === line[1] || line[0].trim() === line[1].trim()) {
+  //         hasReturnInLine[getChangeKey(lineNumber)] = <span style={{color: 'red'}}>maybe a '\n' or '\r' difference</span>;
+  //       }
+  //     }
+  //   }
+  //   return hasReturnInLine;
+  // };
 
   const renderFile = ({oldRevision, newRevision, type, hunks}) => (
-    <Diff key={oldRevision + '-' + newRevision} viewType="split" diffType={type} hunks={hunks}
-          widgets={getWidgets(hunks)}>
+    <Diff key={oldRevision + '-' + newRevision} viewType="split" diffType={type} hunks={hunks}>
       {hunks => hunks.map(hunk => <Hunk key={hunk.content} hunk={hunk}/>)}
     </Diff>
   );
 
   return (
     <div>
-      {files.map(renderFile)}
+      {getStringifiedFiles(files).map(renderFile)}
     </div>
   );
 };
