28f579a855f76cd2546b437718eb01cda8f48e1c
xion <313183373@qq.com>
2019-05-09 20:56:42 +0800

    update home height based on media query

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,17 +5,27 @@ import asyncComponent from '../components/baseComponents/AsyncComponent';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import theme from '../theme';
 import {NoMatch} from "./NoMatchPage";
+import {withStyles} from "@material-ui/core";
 const Problem = asyncComponent(() => import('./problemPage'));
 const Problems = asyncComponent(() => import('./problemsPage'));
 const SignIn = asyncComponent(() => import('./signInPage'));
 const SignUp = asyncComponent(() => import('./signUpPage'));
 const Submits = asyncComponent(() => import('./submitsPage'));
 
-const Home = () => {
+const style = {
+  appBar: {
+    height: 'calc(100vh-64px)',
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100vh-70vh)',
+    },
+  }
+};
+
+const Home = ({classes}) => {
   return (
     <MuiThemeProvider theme={theme}>
       <TopAppBar/>
-      <div style={{height: 'calc(100vh - 70px)'}}>
+      <div className={classes.appBar}>
         <Switch>
           <Route exact path='/' component={Problems}/>
           <Route path='/problem/:id' component={Problem}/>
@@ -29,4 +39,4 @@ const Home = () => {
     </MuiThemeProvider>
   );
 };
-export default Home;
+export default withStyles(style)(Home);
